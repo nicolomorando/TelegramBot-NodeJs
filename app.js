3832a08463dc5c91ecd7a8fefa160f0ad6005bac
Nicolò Morando <morandonic@gmail.com>
2018-02-05 10:44:29 +0100

    Added the hosting user tracker and fixed some problems.

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 ﻿var telegramBot = require('node-telegram-bot-api');
 var sentiment = require('sentiment');
+var username = require('username');
+
+
+// pc station-host target.
+username().then(username => {
+    console.log(username);
+});
+//.
+
 
 var token ='499098435:AAEdqTCTBAsx6-QqY3msQak-2t5azJYCEUU';
 
 var api = new telegramBot(token, {polling: true});
 
+
+
+// author tool startup
 api.onText(/\/author/, function(msg, match) {
   var fromId = msg.from.id;
-  api.sendMessage(fromId, "Il mio sviluppatore si chiama: Morando Nicolò.\nAttualmente sono ancora in fase di sviluppo, è stata aggiornata la versione ed ora posso risponderti dantoti le mie impressioni, tuttavia rimani aggiornato!\nVersione:1.2, Author Website: http://www.fuckjupp.com/");
+  api.sendMessage(fromId, "Il mio sviluppatore si chiama: Morando Nicolò.\nAttualmente sono ancora in fase di sviluppo" + 
+						  ", ma è stata aggiornata la versione ed ora posso risponderti dandoti le mie impressioni, " +
+						  "tuttavia rimani aggiornato!\nVersione: 1.3, hostato sulla WStation dell'utente: '" + username.sync() + "',\nAuthor Website: http://www.fuckjupp.com/");
 });
+// author tool end.
 
+// startup bot
 api.onText(/\/start/, function(msg, match) {
   var fromId = msg.from.id;
   api.sendMessage(fromId, "Ciao! Io sono Fact Inside the bot!" + 
@@ -17,6 +33,8 @@ api.onText(/\/start/, function(msg, match) {
                           "\nCome ad esempio:\n/author\n/vmintelligence" +
 						  "\nPer rivisualizzare il messaggio inserire il comando:\n/start" );
 });
+// startup bot end.
+
 
 var opts = {
   reply_markup: JSON.stringify(
@@ -25,9 +43,12 @@ var opts = {
     }
   )};
 
+  
+// intelligence startup
 api.onText(/\/vmintelligence/, function(msg, match) {
   var fromId = msg.from.id;  
-  api.sendMessage(fromId, "Ok, scrivimi qualcosa (In inglese) così che io possa darti le mie opinioni.", opts)
+  api.sendMessage(fromId, "vmintelligence capisce la sintassi della tua frase in modo che io possa darti un'opinione da me relizzata e concreta."
+						+ "\nScrivimi qualcosa (In inglese) così che io possa darti le mie opinioni.", opts)
   .then(function (sended) {
     var chatId = sended.chat.id;
     var messageId = sended.message_id;
@@ -41,7 +62,8 @@ api.onText(/\/vmintelligence/, function(msg, match) {
 		api.sendMessage(fromId, "Frase neutra, nulla da dire a riguardo.");
 	  }
 	  });
-  });                          
+  });    
+// intelligence startup end.  
                           
 });
 
